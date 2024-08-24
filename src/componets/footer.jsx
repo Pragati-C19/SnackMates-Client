ef5ea30792250b64d84b33d175026faba3b11c6b
Pragati Chothe <pragatichothe@gmail.com>
2024-08-24 23:07:03 +0530

    🍻 footer is set with dummy data

--- src/componets/footer.jsx
+++ src/componets/footer.jsx
@@ -6,14 +6,14 @@ const Footer = () => {
     return (
         <footer className="bg-gray-800 text-white p-4 mt-8">
           <div className="container mx-auto text-center">
-            <p>© 2024 SnackMates. All rights reserved.</p>
-            <div className="flex justify-center space-x-4 mt-2">
+          <div className="flex justify-center space-x-4 mt-2">
               {/* Dummy Social Links */}
+              <a href="#" className="text-indigo-500">Instagram</a>
               <a href="#" className="text-red-500">YouTube</a>
               <a href="#" className="text-blue-500">Facebook</a>
               <a href="#" className="text-blue-400">Twitter</a>
-              <a href="#" className="text-indigo-500">Instagram</a>
             </div>
+            <p>© 2024 SnackMates. All rights reserved.</p>
           </div>
         </footer>
       );
