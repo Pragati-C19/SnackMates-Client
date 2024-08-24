56790ad451061569068d80a65865eb9ab54fea57
Pragati Chothe <pragatichothe@gmail.com>
2024-08-25 01:18:48 +0530

    ♻️ router page added to store all routers

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,14 @@
-import './app.css'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Header from "./componets/header";
-import Footer  from "./componets/footer";
-import HomePage from "./pages/home-page";
-import RestaurantsPage from "./pages/restaurants-page";
-import MenuPage from "./pages/menu-page";
-import FavoritesPage from "./pages/favorites-page";
-import LoginPage from "./pages/login-page";
+import "./app.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import AllRouters from "./router";
 
 function App() {
-
   //  Remember to write className instead of class
   return (
-   <div>
-      <Router>
-      <Header />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/restaurants" element={<RestaurantsPage />} />
-        <Route path="/menus" element={<MenuPage />} />
-        <Route path="/favorites" element={<FavoritesPage />} />
-        <Route path="/login" element={<LoginPage />} />
-      </Routes>
-      <Footer />
-    </Router>
-   </div>
-  
-  )
+    <div>
+      <AllRouters />
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/router.jsx
@@ -0,0 +1,34 @@
+// React Router setup
+
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from "./componets/header";
+import Footer  from "./componets/footer";
+import HomePage from "./pages/home-page";
+import RestaurantsPage from "./pages/restaurants-page";
+import MenuPage from "./pages/menu-page";
+import FavoritesPage from "./pages/favorites-page";
+import LoginPage from "./pages/login-page";
+
+function AllRouters() {
+
+    //  Remember to write className instead of class
+    return (
+     <div>
+        <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/restaurants" element={<RestaurantsPage />} />
+          <Route path="/menus" element={<MenuPage />} />
+          <Route path="/favorites" element={<FavoritesPage />} />
+          <Route path="/login" element={<LoginPage />} />
+        </Routes>
+        <Footer />
+      </Router>
+     </div>
+    
+    )
+  }
+  
+  export default AllRouters
+  
