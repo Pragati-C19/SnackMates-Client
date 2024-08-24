34fd379e41daf07330019fa924fccdc84e0eea7d
Pragati Chothe <pragatichothe@gmail.com>
2024-08-24 23:36:50 +0530

    🍻 routes are added in app for connecting pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
 import './app.css'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from "./componets/header";
+import Footer  from "./componets/footer";
 import HomePage from "./pages/home-page";
+import RestaurantsPage from "./pages/restaurants-page";
 
 function App() {
 
   //  Remember to write className instead of class
   return (
-   
-      <div>
-        <HomePage />
-      </div>
+   <div>
+      <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/restaurants" element={<RestaurantsPage />} />
+      </Routes>
+      <Footer />
+    </Router>
+   </div>
   
   )
 }
