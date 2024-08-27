b94347acf215d800b6e39e475276de7e66d85480
Pragati Chothe <pragatichothe@gmail.com>
2024-08-27 15:41:37 +0530

    👽️ Testing for API calls

--- src/api-calls/restaurants-api.js
+++ src/api-calls/restaurants-api.js
@@ -1,1 +1,23 @@
 // Restaurant-related API calls
+
+import axios from 'axios';
+
+const baseUrl = 'http://localhost:4000/'; // Backend API URL
+
+
+// Get all restaurants http://localhost:4000/restaurants/all
+const getAllRestaurants = async () => {
+  try {
+    const response = await axios.get(`${baseUrl}/restaurants/all`);
+    return response.data;
+  } catch (error) {
+    console.error('Error Fetching Restaurants:', error);
+    throw error; // Handle errors more explicitly
+  }
+};
+
+// Get specific restaurants menu http://localhost:4000/restaurants/menu/{restaurant_id}
+
+//TODO: Similar functions for other API endpoints (e.g., fetchMenuItems, fetchRestaurantDetails)
+
+export default getAllRestaurants
