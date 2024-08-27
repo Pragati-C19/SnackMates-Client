f7ce7b8d73a8d5642c77cd181a8345163a6deda9
Pragati Chothe <pragatichothe@gmail.com>
2024-08-27 15:54:58 +0530

    👽️ Base URl's are set

---
+++ src/api-calls/api-config.jsx
@@ -0,0 +1,9 @@
+// Base URL
+
+const baseUrl = 'http://localhost:4000/'; // Backend API URL
+const authUrl = 'http://localhost:4000/auth'; // Backend auth API URL
+const restaurantsUrl = 'http://localhost:4000/restaurants/'; // Backend restaurants API URL
+const menuUrl = 'http://localhost:4000/menus/'; // Backend menus API URL
+const favoritesUrl = 'http://localhost:4000/favorites/'; // Backend favorites API URL
+
+export default {baseUrl, authUrl, restaurantsUrl, menuUrl, favoritesUrl}
