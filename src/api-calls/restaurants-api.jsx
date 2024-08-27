3a775e1c08d228fccd7a9c0295e35f41756cb9fc
Pragati Chothe <pragatichothe@gmail.com>
2024-08-27 16:02:07 +0530

    👽️ restaurants api's are set, tesing remain

---
+++ src/api-calls/restaurants-api.jsx
@@ -0,0 +1,28 @@
+// Restaurant-related API calls
+
+import axios from 'axios';
+import restaurantsUrl from './api-config'; // Import API_URL from api-config.jsx
+
+// Get all restaurants http://localhost:4000/restaurants/all
+const getAllRestaurants = async () => {
+  try {
+    const response = await axios.get(`${restaurantsUrl}/all`);
+    return response.data;
+  } catch (error) {
+    console.error('Error Fetching Restaurants:', error);
+    throw error; // Handle errors more explicitly
+  }
+};
+
+// Get specific restaurants menu http://localhost:4000/restaurants/menu/{restaurant_id}
+const getRestaurantsByID = async () => {
+  try {
+    const response = await axios.get(`${restaurantsUrl}/menu/${restaurantId}`);
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching restaurant menu:', error);
+    throw error;
+  }
+};
+
+export default {getAllRestaurants, getRestaurantsByID}
