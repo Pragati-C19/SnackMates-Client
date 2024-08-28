b0dcad806d5dbe4b00aabbe0eddf0728e4c663ce
Pragati Chothe <pragatichothe@gmail.com>
2024-08-28 16:20:34 +0530

    💄updated navbar a little bit

--- src/componets/header.jsx
+++ src/componets/header.jsx
@@ -1,28 +1,33 @@
 // Header componets
 
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import Logo from '../imgs/Logo.png'
 
 const Header = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+  //TODO: Search query is need to be added which I have store in search-bar.js for now
+
   return (
-    <header className="bg-gradient-to-r from-green-200 to-yellow-300 p-4">
-      <div className="container mx-auto flex justify-between items-center">
-        <div className="text-3xl font-bold text-purple-800">SnackMates</div>
-        <input
+    <nav className="bg-gray-800 text-white p-4 flex justify-between">
+      <div className="flex items-center">
+        <img src={Logo} alt="Logo" className="mx-auto h-8 w-auto px-6" />
+      </div>
+      <input
           type="text"
           placeholder="Feelin’ Snacky? Type and Attack-y!..."
-          className="rounded p-2 w-1/3"
+          className="block w-1/4 rounded-md border-0 py-2 px-4 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm" 
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
         />
-        <nav>
-            <Link to="/" className="hover:text-purple-800 cursor-pointer">Home</Link>
-            <Link to="/restaurants" className="hover:text-purple-800 cursor-pointer">Restaurants</Link>
-            <Link to="/menus" className="hover:text-purple-800 cursor-pointer">Menus</Link>
-            <Link to="/favorites" className="hover:text-purple-800 cursor-pointer">Favorites</Link>
-            <Link to="#" className="hover:text-purple-800 cursor-pointer">Cart</Link>
-            <Link to="/login" className="hover:text-purple-800 cursor-pointer">Login</Link>
-        </nav>
+      <div className="flex space-x-10">
+        <a href="/" className="py-2 hover:text-gray-300">Home</a>
+        <a href="/restaurants" className="py-2 hover:text-gray-300">Restaurants</a>
+        <a href="/menus" className="py-2 hover:text-gray-300">Menu</a>
+        <a href="/favorites" className="py-2 hover:text-gray-300">Favorites</a>
+        <a href="/cart" className="py-2 hover:text-gray-300">Cart</a>
+        <a href="/login" className="bg-white text-black px-3 py-2 rounded-md h-10 hover:bg-blue-100">Login</a>
       </div>
-    </header>
+    </nav>
   );
 };
 
--- src/componets/search-bar.js
+++ src/componets/search-bar.js
@@ -1,1 +1,8 @@
 // Search bar component
+
+ // query for search bar this will reflect in header again 
+ const [query, setQuery] = useState("");
+
+ filteredList = rows.filter((item) => {
+   return item.title.toLowerCase().includes(query.toLowerCase());
+ });
