685625599d3da67ace221d613f8335156b8dfe52
Pragati Chothe <pragatichothe@gmail.com>
2024-08-28 22:40:11 +0530

    💄 UI for footer is set

--- src/componets/footer.jsx
+++ src/componets/footer.jsx
@@ -3,20 +3,31 @@
 import React from 'react';
 
 const Footer = () => {
-    return (
-        <footer className="bg-gray-800 text-white p-4 mt-8">
-          <div className="container mx-auto text-center">
-          <div className="flex justify-center space-x-4 mt-2">
-              {/* Dummy Social Links */}
-              <a href="#" className="text-indigo-500">Instagram</a>
-              <a href="#" className="text-red-500">YouTube</a>
-              <a href="#" className="text-blue-500">Facebook</a>
-              <a href="#" className="text-blue-400">Twitter</a>
-            </div>
-            <p>© 2024 SnackMates. All rights reserved.</p>
-          </div>
-        </footer>
-      );
+  return (
+    <footer className="bg-gray-800 text-white py-6 px-6">
+  <div className="text-center">
+  <div className="flex justify-center space-x-7 mb-4 text-2xl">
+      <a href="#" className="text-blue-500">
+        <i className="fa-brands fa-facebook-f"></i>
+      </a>
+      <a href="#" className="text-pink-500">
+        <i className="fa-brands fa-instagram"></i>
+      </a>
+      <a href="#" className="text-blue-400">
+        <i className="fa-brands fa-twitter"></i>
+      </a>
+      <a href="#" className="text-red-600">
+        <i className="fa-brands fa-youtube"></i>
+      </a>
+    </div>
+    <p className="text-sm">
+      with 💖 pragati_c19, &copy; 2024 SnackMates. All rights reserved.
+    </p>
+    
+  </div>
+</footer>
+
+  );
 };
 
 export default Footer;
