c8dd9194964134b5ab8032bea53b18e9410451c0
Pragati Chothe <pragatichothe@gmail.com>
2024-08-31 14:57:24 +0530

    ⚡️ change it as DRY Principle

--- src/api-calls/api-config.jsx
+++ src/api-calls/api-config.jsx
@@ -1,10 +1,10 @@
 // Base URL
 
 const snackmates_base_url = 'http://localhost:4000'; // Backend API URL
-const authUrl = 'http://localhost:4000/auth'; // Backend auth API URL
-const restaurantsUrl = 'http://localhost:4000/restaurants/'; // Backend restaurants API URL
-const menuUrl = 'http://localhost:4000/menus/'; // Backend menus API URL
-const favoritesUrl = 'http://localhost:4000/favorites'; // Backend favorites API URL
-const cartURL = 'http://localhost:4000/menus/'; // Backend cart API URL
+const authUrl = `${snackmates_base_url}/auth`; // Backend auth API URL
+const restaurantsUrl = `${snackmates_base_url}/restaurants`; // Backend restaurants API URL
+const menuUrl = `${snackmates_base_url}/menus`; // Backend menus API URL
+const favoritesUrl = `${snackmates_base_url}/favorites`; // Backend favorites API URL
+const cartURL = `${snackmates_base_url}/cart`; // Backend cart API URL
 
 export default snackmates_base_url
